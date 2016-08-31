a7402b7440a9475a32c9119abc9f6bf2a60c7019
Charles J Dillon <charles.dillon2@wsu.edu>
2016-08-31 16:21:17 -0700

    Change font size. Change font weight. Test search bar. Change yellow to red.

--- functions.php
+++ functions.php
@@ -10,4 +10,13 @@ function wsu_labs_chandler_enqueue_scripts() {
 /*
 * Add HTML5 search box on the side bar menu
 */
-add_theme_support( 'html5', array( 'sb-search' ) );
+add_theme_support( 'html5', array( 'search-form' ) );
+
+add_filter( 'get_search_form', 'search_form_placeholder' );
+function search_form_placeholder( $html ) {
+    /*if ( current_theme_supports( 'html5', 'search-form' ) ) {
+        $html = str_replace( 'placeholder="Change me"', 'placeholder="New Text"', $html );
+    }*/
+
+    return $html;
+}
